a6fe43429227d65ab9469d369617a2d00b01cd35
nicolasmahlangu75 <nicolasmahlangu75@gmail.com>
2020-08-27 11:25:45 +0200

    Working version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -163,7 +163,8 @@ const routes: Routes = [
    {
     path: 'stripe-web',
     loadChildren: () => import('./pages/stripe-web/stripe-web.module').then( m => m.StripeWebPageModule)
-   },  {
+   },
+  {
     path: 'edit',
     loadChildren: () => import('./edit/edit.module').then( m => m.EditPageModule)
   },
@@ -172,14 +173,9 @@ const routes: Routes = [
     loadChildren: () => import('./invoice/invoice.module').then( m => m.InvoicePageModule)
   },
   {
-    path: 'invoice-home',
-    loadChildren: () => import('./invoice-home/invoice-home.module').then( m => m.InvoiceHomePageModule)
-  },
-  {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   }
-
 
 ];
 @NgModule({
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import { DataService } from '../..';
+import { DataService } from '../../data.service.ts.service';
 
 @Component({
   selector: 'app-home',
